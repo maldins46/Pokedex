588b8384850256d666cdf6f8d45b22d45930164b
Riccardo Maldini <riccardo.maldini@gmail.com>
2025-03-06 22:00:47 +0100

    Boilerplate structure for Spring Boot project

---
+++ src/test/java/it/riccardomaldini/Pokedex/PokedexApplicationTests.java
@@ -0,0 +1,13 @@
+package it.riccardomaldini.Pokedex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
